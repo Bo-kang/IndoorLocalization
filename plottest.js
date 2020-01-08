f7a28406d9a9ea996874e419847cad2690c27a81
Bo-kang <53891564+Bo-kang@users.noreply.github.com>
2020-01-08 15:55:34 +0900

    Delete plottest.js

--- plottest.js
+++
@@ -1,9 +0,0 @@
-var plotly = require('')
-var fs = require('fs')
-
-var valueFromFile
-valueFromFile = fs.readFileSync('2020-01-06_First_1.csv', 'utf-8')
-var RA = []
-var x = []
-var temp = new Map()
-var lines = valueFromFile.split('\n')
